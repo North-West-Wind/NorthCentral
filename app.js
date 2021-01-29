98f621fb12331e47545a28fcbc0c175f3f803bcf
NorthWestWind <wsyn148@gmail.com>
2021-01-29 19:11:18 +0800

    Trying chrome-aws-lambda

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const puppeteer = require("puppeteer");
+const chromium = require("chrome-aws-lambda");
 var browser;
-const options = { args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--single-process', '--disable-gpu', "--proxy-server='direct://'", '--proxy-bypass-list=*'], headless: true }
 
 app.get('/', async (req, res) => {
     if (!req.query.code) return res.json({ error: "No code input." });
+    if (!browser) return res.json({ error: "Browser hasn't been initialized." });
     try {
-        const result = await (Object.getPrototypeOf(async function () { }).constructor("browser", req.query.code))(browser);
+        const result = await(Object.getPrototypeOf(async function () { }).constructor("browser", req.query.code))(browser);
         res.json({ result, error: null });
     } catch (err) {
         res.json({ error: err.message });
@@ -15,6 +15,12 @@ app.get('/', async (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, async () => {
-    browser = await puppeteer.launch(options);
+    browser = await chromium.puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--single-process', '--disable-gpu', "--proxy-server='direct://'", '--proxy-bypass-list=*'],
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+    });
     console.log("Browser Initialized!");
 });
