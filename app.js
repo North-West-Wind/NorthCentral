fcb476839cec4822524d2df7210daf0f4b560ca9
NorthWestWind <45654842+North-West-Wind@users.noreply.github.com>
2021-01-29 18:48:17 +0800

    "async"

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const puppeteer = require("puppeteer");
 var browser;
 const options = { args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--single-process', '--disable-gpu', "--proxy-server='direct://'", '--proxy-bypass-list=*'], headless: true }
 
-app.get('/', function (req, res) {
+app.get('/', async (req, res) => {
     if (!req.query.code) return res.json({ error: "No code input." });
     try {
         const result = await (Object.getPrototypeOf(async function () { }).constructor("browser", req.query.code))(browser);
