f9fffe80fbe6662a96bc28f26a4ade9b5660e88d
NorthWestWind <wsyn148@gmail.com>
2021-01-29 20:06:10 +0800

    Tried another code

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const app = express();
-const chromium = require("chrome-aws-lambda");
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 var browser;
-(async() => {
-    browser = await chromium.puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--single-process', '--disable-gpu', "--proxy-server='direct://'", '--proxy-bypass-list=*'],
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath,
-        headless: true,
-        ignoreHTTPSErrors: true,
+async function launchBrowser() {
+    browser = await puppeteer.launch({
+        args: chrome.args.concat(['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--single-process', '--disable-gpu', "--proxy-server='direct://'", '--proxy-bypass-list=*']),
+        executablePath: await chrome.executablePath,
+        headless: false
     });
     console.log("Browser Initialized!");
-})();
+}
 
 app.get('/', async (req, res) => {
     if (!req.query.code) return res.json({ error: "No code input." });
-    if (!browser) return res.json({ error: "Browser hasn't been initialized." });
+    if (!browser) {
+        await launchBrowser();
+        return res.json({ error: "Browser hasn't been initialized." });
+    }
     try {
-        const result = await(Object.getPrototypeOf(async function () { }).constructor("browser", req.query.code))(browser);
+        const page = await browser.newPage();
+        const result = await(Object.getPrototypeOf(async function () { }).constructor("page", req.query.code))(page);
         res.json({ result, error: null });
+        await page.close();
     } catch (err) {
         res.json({ error: err.message });
     }
