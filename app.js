5417891828615f918920c5027a796b3bc18415c4
NorthWestWind <45654842+North-West-Wind@users.noreply.github.com>
2021-01-31 10:20:12 +0800

    Back to normal

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
-const processing = {};
-var ID = () => '_' + Math.random().toString(36).substr(2, 9);
 var browser;
 async function launchBrowser() {
-    const chrome = require('chrome-aws-lambda');
-    const puppeteer = require('puppeteer-core');
+    const puppeteer = require('puppeteer');
     browser = await puppeteer.launch({
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--single-process', '--disable-gpu', "--proxy-server='direct://'", '--proxy-bypass-list=*'],
-        headless: false,
-        executablePath: await chrome.executablePath
+        headless: false
     });
     console.log("Browser Initialized!");
 }
@@ -17,33 +13,14 @@ async function launchBrowser() {
 app.get('/', async (req, res) => {
     if (!req.query.code) return res.json({ error: "No code input." });
     if (req.query.code.includes("browser.close()")) return res.json({ error: "Don't you dare closing my browser." });
-    const id = ID();
-    processing[id] = {
-        running: true,
-        result: null,
-        error: null
-    };
-    res.json({ id, error: null });
     try {
         if (!browser) await launchBrowser();
         const page = await browser.newPage();
         const result = await (Object.getPrototypeOf(async function () { }).constructor("page", req.query.code))(page);
         await page.close();
-        processing[id].result = result;
     } catch (err) {
-        processing[id].error = err.message;
+        res.json({ error: err.message });
     }
-    processing[id].running = false;
-});
-
-app.get('/result', async (req, res) => {
-    if (!req.query.id) return res.json({ error: "No ID received." });
-    const process = processing[req.query.id];
-    return res.json(process);
-    if (!process) return res.json({ error: "ID not found." });
-    if (process.running) return res.json({ error: "Run is not completed." });
-    res.json(processing[req.query.id]);
-    delete processing[req.query.id];
 });
 
 const server = app.listen(process.env.PORT || 3000, async () => {
