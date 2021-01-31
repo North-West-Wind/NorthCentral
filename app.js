ca34014b42b71fedc8210dcafa13dc39caf23de7
North-2 <78286840+North-2@users.noreply.github.com>
2021-01-31 10:54:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const processes = {};
+const ID = () => "_" + Math.random().toString(36).substr(2, 9);
 var browser;
 async function launchBrowser() {
     const puppeteer = require('puppeteer');
@@ -13,15 +15,35 @@ async function launchBrowser() {
 app.get('/', async (req, res) => {
     if (!req.query.code) return res.json({ error: "No code input." });
     if (req.query.code.includes("browser.close()")) return res.json({ error: "Don't you dare closing my browser." });
+    const id = ID();
+    res.json({ id, error: null });
+    processes[id] = {
+        running: true,
+        result: null,
+        error: null
+    };
+    console.log("Code Received");
     try {
         if (!browser) await launchBrowser();
         const page = await browser.newPage();
+        console.log("Running Code");
         const result = await (Object.getPrototypeOf(async function () { }).constructor("page", req.query.code))(page);
         await page.close();
-        res.json({ result, error: null })
+        processes[id].result = result;
+        console.log("Code Finished");
     } catch (err) {
-        res.json({ error: err.message });
+        processes[id].error = err.message;
+        console.log("Code Finished with Error");
     }
+    processes[id].running = false;
+});
+
+app.get("/result", (req, res) => {
+    if (!req.query.id) return res.json({ error: "Please send an ID." });
+    const process = processes[req.query.id];
+    if (!process) return res.json({ error: "ID not found." });
+    if (process.running) return res.json({ error: "Process is running." });
+    res.json(process);
 });
 
 const server = app.listen(process.env.PORT || 3000, async () => {
