de941da265fd52a474503a2f406fdeafa04a0c4e
North-West-Wind <wsyn148@gmail.com>
2022-09-01 22:08:59 +0800

    TradeW1nd invite link

--- src/index.ts
+++ src/index.ts
@@ -83,6 +83,22 @@ app.get("/api/curseforge/mods/:id/files/:fileId", async (req, res) => {
 	}
 });
 
+app.get("/bot/tradew1nd/invite", async(req, res) => {
+	try {
+		const response = await fetch(`http://pi-api.ddns.net:3000/`);
+		if (!response.ok) res.sendStatus(response.status);
+		else {
+			const data = <any>await response.json();
+			for (const bot of data) {
+				if (bot.size < 100) return res.redirect(301, `https://discord.com/api/oauth2/authorize?client_id=${bot.id}&permissions=2150755392&scope=bot%20applications.commands`);
+			}
+			res.send("All TradeW1nd derivatives are full!");
+		}
+	} catch (err) {
+		if (!res.headersSent) res.sendStatus(500);
+	}
+});
+
 app.get("/:page", (req, res, next) => {
 	if (!PAGES.includes(req.params.page)) next();
 	else {
