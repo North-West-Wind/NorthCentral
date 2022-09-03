80f14a766f957d9348283f2689496966de1a6b1f
North-West-Wind <wsyn148@gmail.com>
2022-09-03 12:58:00 +0800

    env output dir

--- api/index.ts
+++ api/index.ts
@@ -18,17 +18,18 @@ const PAGES = [
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.set('views', process.env.OUTDIR + "views")
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
 	if (!parseInt(req.cookies.no_3d) && !req.query.no_3d) res.render("index", { page: 0 });
 	else res.render("plain", { page: 0 });
 });
-app.get("/n0rthwestw1nd/manual", (req, res) => res.sendFile("public/assets/safe_manual.pdf", { root: "." }));
+app.get("/n0rthwestw1nd/manual", (req, res) => res.sendFile("public/assets/safe_manual.pdf", { root: process.env.OUTDIR }));
 app.get("/n0rthwestw1nd/manual/:ver", (req, res) => {
-	if (req.params.ver === "unsafe") res.sendFile("public/assets/unsafe_manual.pdf", { root: "." });
-	else if (req.params.ver === "tradew1nd") res.sendFile("public/assets/tradew1nd_manual.pdf", { root: "." });
-	else res.sendFile("public/assets/safe_manual.pdf", { root: "." });
+	if (req.params.ver === "unsafe") res.sendFile("public/assets/unsafe_manual.pdf", { root: process.env.OUTDIR });
+	else if (req.params.ver === "tradew1nd") res.sendFile("public/assets/tradew1nd_manual.pdf", { root: process.env.OUTDIR });
+	else res.sendFile("public/assets/safe_manual.pdf", { root: process.env.OUTDIR });
 });
 app.get("/api/ping", (_req, res) => {
 	res.sendStatus(200);
