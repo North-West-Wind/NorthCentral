84d6a347ff620e4921e9edcf52dfb6293617a45f
NorthWestWind <wsyn148@gmail.com>
2022-10-31 16:06:29 +0800

    some more sections

--- public/js/tradew1nd/listener.js
+++ public/js/tradew1nd/listener.js
@@ -1,3 +1,21 @@
+const invite = document.createElement("a");
+invite.href = "/tradew1nd/invite";
+invite.target = "invite";
+invite.setAttribute("top-component", 1);
+invite.text = "Invite";
+
 window.onresize = () => {
-    
+    var ratio = window.innerWidth / window.innerHeight;
+    if (ratio < 1) disableStylesheet(document.getElementById("horizontal"));
+    else disableStylesheet(document.getElementById("vertical"));
+    if (document.getElementById("vertical").disabled) {
+        document.getElementById("home-link").href = "/tradew1nd";
+        const topbar = document.getElementById("topbar");
+        topbar.append(invite);
+    } else {
+        document.getElementById("home-link").href = "#";
+        document.querySelectorAll("[top-component=1]").forEach(e => e.remove());
+    }
 }
+
+window.onresize();
