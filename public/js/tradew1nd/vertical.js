16715f6aab194a7aee9c56901bae8f79f17926d5
NorthWestWind <wsyn148@gmail.com>
2022-11-04 14:10:33 +0800

    vertical menu

--- public/js/tradew1nd/vertical.js
+++ public/js/tradew1nd/vertical.js
@@ -1,6 +1,8 @@
-document.getElementById("home-image").onclick = () => {
+document.getElementById("home-link").onclick = (event) => {
     if (document.getElementById("vertical").disabled) return;
+    event.stopPropagation();
     const menu = document.getElementById("side-menu");
     if (menu.style.width) menu.style.width = "";
     else menu.style.width = "100vw";
 }
+
