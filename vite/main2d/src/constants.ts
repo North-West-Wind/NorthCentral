facd87067a92415a3f275c965e939fdbfdbff186
North-West-Wind <wsyn148@gmail.com>
2024-05-12 00:56:16 +0800

    bot and sheet floor

--- vite/main2d/src/constants.ts
+++ vite/main2d/src/constants.ts
@@ -1,6 +1,8 @@
 import AutoFishFloor from "./floors/auto_fish";
 import GroundFloor from "./floors/ground";
 import MoreBootsFloor from "./floors/more_boots";
+import N0rthWestW1ndFloor from "./floors/n0rthwestw1nd";
+import SheetMusicFloor from "./floors/sheet_music";
 import SkyFarmFloor from "./floors/sky_farm";
 import Floor from "./types/floor";
 
@@ -15,3 +17,5 @@ addFloor(new GroundFloor());
 addFloor(new AutoFishFloor());
 addFloor(new MoreBootsFloor());
 addFloor(new SkyFarmFloor());
+addFloor(new N0rthWestW1ndFloor());
+addFloor(new SheetMusicFloor());
---
+++ vite/main2d/src/floors/n0rthwestw1nd.ts
@@ -0,0 +1,7 @@
+import Floor from "../types/floor";
+
+export default class N0rthWestW1ndFloor extends Floor {
+	constructor() {
+		super("n0rthwestw1nd", 4);
+	}
+}
